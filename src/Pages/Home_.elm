d0dd4d1b5457185a9b91b3e89244a2080841c2a5
user <user@example.com>
2021-05-26 13:31:57 +0200

    cleanup dead code

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -29,8 +29,7 @@ page shared req =
 
 
 type alias Model =
-    { people : List Person
-    , tableState : Table.State
+    { tableState : Table.State
     , spieleRequest : RequestByKategorieState
     }
 
@@ -49,8 +48,7 @@ type RequestByKategorieState
 
 init : ( Model, Cmd Msg )
 init =
-    ( { people = presidents
-      , tableState = Table.initialSort "Year"
+    ( { tableState = Table.initialSort "Year"
       , spieleRequest = ByKategorieLoading
       }
     , Http.get
@@ -65,25 +63,13 @@ init =
 
 
 type Msg
-    = ReplaceMe
-    | Noop (Result Http.Error String)
-    | SetTableState Table.State
+    = SetTableState Table.State
     | SpieleByKategorieReceived (Result Http.Error (List KategorieTupel))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    let
-        noChange =
-            ( model, Cmd.none )
-    in
     case msg of
-        ReplaceMe ->
-            noChange
-
-        Noop result ->
-            ( model, Cmd.none )
-
         SetTableState newState ->
             ( { model | tableState = newState }
             , Cmd.none
@@ -113,12 +99,11 @@ subscriptions model =
 
 view : Model -> View Msg
 view model =
-    --View.placeholder "GetJson"
     { title = "GetJson"
     , body =
         List.map toUnstyled
             [ let
-                { people, tableState, spieleRequest } =
+                { tableState, spieleRequest } =
                     model
               in
               case spieleRequest of
@@ -200,21 +185,9 @@ tableConfig =
             , Table.stringColumn "Spieler" spieleranzahl
             , Table.intColumn "Alter" .altersempfehlung
             , Table.stringColumn "Kategorie" kategorie
-
-            --, columnErscheinungsjahr
-            --, Table.stringColumn "City" .city
-            --, Table.stringColumn "State" .state
             ]
         , customizations = customizations
         }
-
-
-type alias Person =
-    { name : String
-    , year : Int
-    , city : String
-    , state : String
-    }
 
 
 flattenSpiele : List KategorieTupel -> List Spiel
@@ -225,267 +198,3 @@ flattenSpiele byKatgorie =
             Tuple.second tuple
     in
     List.concatMap getSpieleList byKatgorie
-
-
-presidents : List Person
-presidents =
-    [ Person "George Washington" 1732 "Westmoreland County" "Virginia"
-    , Person "John Adams" 1735 "Braintree" "Massachusetts"
-    , Person "John Adams" 1735 "Braintree" "Massachusetts"
-    , Person "John Adams" 1735 "Braintree" "Massachusetts"
-    , Person "John Adams" 1735 "Braintree" "Massachusetts"
-    , Person "John Adams" 1735 "Braintree" "Massachusetts"
-    , Person "John Adams" 1735 "Braintree" "Massachusetts"
-    , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
-    , Person "James Madison" 1751 "Port Conway" "Virginia"
-    , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
-    , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
-    , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
-    , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
-    , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
-    , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
-    , Person "John Tyler" 1790 "Charles City County" "Virginia"
-    , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
-    , Person "James K. Polk" 1795 "Pineville" "North Carolina"
-    , Person "Millard Fillmore" 1800 "Summerhill" "New York"
-    , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
-    , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
-    , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
-    , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
-    , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
-    , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
-    , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
-    , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
-    , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
-    , Person "William McKinley" 1843 "Niles" "Ohio"
-    , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
-    , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
-    , Person "Theodore Roosevelt" 1858 "New York City" "New York"
-    , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
-    , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
-    , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
-    , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
-    , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
-    , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
-    , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
-    , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
-    , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
-    , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
-    , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
-    , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
-    , Person "Jimmy Carter" 1924 "Plains" "Georgia"
-    , Person "George W. Bush" 1946 "New Haven" "Connecticut"
-    , Person "Bill Clinton" 1946 "Hope" "Arkansas"
-    , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
-    , Person "Donald Trump" 1946 "New York City" "New York"
-    , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
-    , Person "James Madison" 1751 "Port Conway" "Virginia"
-    , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
-    , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
-    , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
-    , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
-    , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
-    , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
-    , Person "John Tyler" 1790 "Charles City County" "Virginia"
-    , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
-    , Person "James K. Polk" 1795 "Pineville" "North Carolina"
-    , Person "Millard Fillmore" 1800 "Summerhill" "New York"
-    , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
-    , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
-    , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
-    , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
-    , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
-    , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
-    , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
-    , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
-    , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
-    , Person "William McKinley" 1843 "Niles" "Ohio"
-    , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
-    , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
-    , Person "Theodore Roosevelt" 1858 "New York City" "New York"
-    , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
-    , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
-    , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
-    , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
-    , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
-    , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
-    , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
-    , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
-    , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
-    , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
-    , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
-    , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
-    , Person "Jimmy Carter" 1924 "Plains" "Georgia"
-    , Person "George W. Bush" 1946 "New Haven" "Connecticut"
-    , Person "Bill Clinton" 1946 "Hope" "Arkansas"
-    , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
-    , Person "Donald Trump" 1946 "New York City" "New York"
-    , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
-    , Person "James Madison" 1751 "Port Conway" "Virginia"
-    , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
-    , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
-    , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
-    , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
-    , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
-    , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
-    , Person "John Tyler" 1790 "Charles City County" "Virginia"
-    , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
-    , Person "James K. Polk" 1795 "Pineville" "North Carolina"
-    , Person "Millard Fillmore" 1800 "Summerhill" "New York"
-    , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
-    , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
-    , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
-    , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
-    , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
-    , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
-    , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
-    , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
-    , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
-    , Person "William McKinley" 1843 "Niles" "Ohio"
-    , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
-    , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
-    , Person "Theodore Roosevelt" 1858 "New York City" "New York"
-    , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
-    , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
-    , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
-    , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
-    , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
-    , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
-    , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
-    , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
-    , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
-    , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
-    , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
-    , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
-    , Person "Jimmy Carter" 1924 "Plains" "Georgia"
-    , Person "George W. Bush" 1946 "New Haven" "Connecticut"
-    , Person "Bill Clinton" 1946 "Hope" "Arkansas"
-    , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
-    , Person "Donald Trump" 1946 "New York City" "New York"
-    , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
-    , Person "James Madison" 1751 "Port Conway" "Virginia"
-    , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
-    , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
-    , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
-    , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
-    , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
-    , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
-    , Person "John Tyler" 1790 "Charles City County" "Virginia"
-    , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
-    , Person "James K. Polk" 1795 "Pineville" "North Carolina"
-    , Person "Millard Fillmore" 1800 "Summerhill" "New York"
-    , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
-    , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
-    , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
-    , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
-    , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
-    , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
-    , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
-    , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
-    , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
-    , Person "William McKinley" 1843 "Niles" "Ohio"
-    , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
-    , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
-    , Person "Theodore Roosevelt" 1858 "New York City" "New York"
-    , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
-    , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
-    , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
-    , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
-    , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
-    , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
-    , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
-    , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
-    , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
-    , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
-    , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
-    , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
-    , Person "Jimmy Carter" 1924 "Plains" "Georgia"
-    , Person "George W. Bush" 1946 "New Haven" "Connecticut"
-    , Person "Bill Clinton" 1946 "Hope" "Arkansas"
-    , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
-    , Person "Donald Trump" 1946 "New York City" "New York"
-    , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
-    , Person "James Madison" 1751 "Port Conway" "Virginia"
-    , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
-    , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
-    , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
-    , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
-    , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
-    , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
-    , Person "John Tyler" 1790 "Charles City County" "Virginia"
-    , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
-    , Person "James K. Polk" 1795 "Pineville" "North Carolina"
-    , Person "Millard Fillmore" 1800 "Summerhill" "New York"
-    , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
-    , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
-    , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
-    , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
-    , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
-    , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
-    , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
-    , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
-    , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
-    , Person "William McKinley" 1843 "Niles" "Ohio"
-    , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
-    , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
-    , Person "Theodore Roosevelt" 1858 "New York City" "New York"
-    , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
-    , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
-    , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
-    , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
-    , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
-    , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
-    , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
-    , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
-    , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
-    , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
-    , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
-    , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
-    , Person "Jimmy Carter" 1924 "Plains" "Georgia"
-    , Person "George W. Bush" 1946 "New Haven" "Connecticut"
-    , Person "Bill Clinton" 1946 "Hope" "Arkansas"
-    , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
-    , Person "Donald Trump" 1946 "New York City" "New York"
-    , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
-    , Person "James Madison" 1751 "Port Conway" "Virginia"
-    , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
-    , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
-    , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
-    , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
-    , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
-    , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
-    , Person "John Tyler" 1790 "Charles City County" "Virginia"
-    , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
-    , Person "James K. Polk" 1795 "Pineville" "North Carolina"
-    , Person "Millard Fillmore" 1800 "Summerhill" "New York"
-    , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
-    , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
-    , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
-    , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
-    , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
-    , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
-    , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
-    , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
-    , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
-    , Person "William McKinley" 1843 "Niles" "Ohio"
-    , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
-    , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
-    , Person "Theodore Roosevelt" 1858 "New York City" "New York"
-    , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
-    , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
-    , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
-    , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
-    , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
-    , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
-    , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
-    , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
-    , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
-    , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
-    , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
-    , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
-    , Person "Jimmy Carter" 1924 "Plains" "Georgia"
-    , Person "George W. Bush" 1946 "New Haven" "Connecticut"
-    , Person "Bill Clinton" 1946 "Hope" "Arkansas"
-    , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
-    , Person "Donald Trump" 1946 "New York City" "New York"
-    ]
